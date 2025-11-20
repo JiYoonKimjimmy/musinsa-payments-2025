e19cfffe5b8a2ef3dab272ffc29e581987964aa2
jimmyberg <jimmyberg.kim@gmail.com>
2025-11-20 21:43:20 +0900

    feat: spring project 초기 구성

---
+++ src/main/kotlin/com/musinsa/payments/MusinsaPaymentsApplication.kt
@@ -0,0 +1,11 @@
+package com.musinsa.payments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusinsaPaymentsApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusinsaPaymentsApplication>(*args)
+}
